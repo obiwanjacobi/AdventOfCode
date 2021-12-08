33a10133842402c27a7d3c864a6011590b7526d2
Marc Jacobi <marc.jacobi@macaw.nl>
2021-12-08 17:00:40 +0100

    day 7

--- 2021/AdventOfCode21.Day7/Program.fs
+++ 2021/AdventOfCode21.Day7/Program.fs
@@ -5,6 +5,61 @@ open System;
 // part1
 
 let lines = System.IO.File.ReadLines "input.txt" |> Seq.toArray
-//let lines = System.IO.File.ReadLines "test.txt" |> Seq.toArray
-//lines |> Seq.iter(fun x -> printfn "%s" x)
-//printfn "Number of input lines: %d" (lines |> Seq.length)
+//let lines = System.IO.File.ReadLines "example.txt" |> Seq.toArray
+
+let values = 
+    (lines |> Seq.head).Split ','
+    |> Seq.map Int32.Parse
+    |> Seq.toArray
+
+
+printfn "%d values" (Seq.length values)
+printfn ""
+
+let positions = 
+    [values |> Seq.min .. values |> Seq.max]
+
+let calcFuelByPos target =
+    let diff pos =
+        abs (target - pos)
+    
+    values
+    |> Seq.map diff
+    |> Seq.sum
+    
+let (position1, fuel1) =
+    let totalFuelForPos (pos, minFuel) value =
+        let fuel = calcFuelByPos value
+        if fuel < minFuel then
+            (value, fuel)
+        else
+            (pos, minFuel)
+
+    positions |> Seq.fold totalFuelForPos (0, Int32.MaxValue)
+
+printfn "1) Best position %d for %d Fuel." position1 fuel1      // fuel=356922
+
+// part2
+
+let calcFuelByPos2 target =
+    let diff pos =
+        let len = abs (target - pos)
+        // had to look this one up
+        // n-th triangular number: (len * (len+1))/2
+        (len * len + len) / 2
+    
+    values
+    |> Seq.map diff
+    |> Seq.sum
+
+let (position2, fuel2) =
+    let totalFuelForPos (pos, minFuel) value =
+        let fuel = calcFuelByPos2 value
+        if fuel < minFuel then
+            (value, fuel)
+        else
+            (pos, minFuel)
+
+    positions |> Seq.fold totalFuelForPos (0, Int32.MaxValue)
+
+printfn "2) Best position %d for %d Fuel." position2 fuel2      // fuel=100347031
