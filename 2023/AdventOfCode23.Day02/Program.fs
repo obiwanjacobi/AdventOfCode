d96d43c7c9d3d8fba48d60ae8054e29b89a73183
Marc Jacobi <Marc.Jacobi@macaw.nl>
2023-12-01 19:49:11 +0100

    add day 2 template

---
+++ 2023/AdventOfCode23.Day02/Program.fs
@@ -0,0 +1,9 @@
+﻿// https://adventofcode.com/2023/day/0
+
+// part1
+
+let lines = System.IO.File.ReadLines "sample.txt"
+//let lines = System.IO.File.ReadLines "input.txt"
+//lines |> Seq.iter(fun x -> printfn "%s" x)
+//let numberOfLines = lines |> Seq.length
+//printfn "Number of input lines: %d" numberOfLines
