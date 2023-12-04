5295b14306badef4d2d62476acefe98b98438f73
Marc Jacobi <Marc.Jacobi@macaw.nl>
2023-12-04 16:21:44 +0100

    day 4 part 1

--- 2023/AdventOfCode23.Day04/Program.fs
+++ 2023/AdventOfCode23.Day04/Program.fs
@@ -1,5 +1,44 @@
 ﻿// https://adventofcode.com/2023/day/4
+
+//let lines = System.IO.File.ReadLines "sample.txt"
+let lines = System.IO.File.ReadLines "input.txt"
+
+// input parsing
+
+type Card = 
+  { Id: int; Winning: int array; Numbers: int array; }
+
+let parseNumbers (line: string) = 
+  line.Trim().Split(' ', System.StringSplitOptions.RemoveEmptyEntries) 
+  |> Array.map (fun n -> n |> int)
+
+let parseCard (line: string) =
+  let parts = line.Split(':')
+  let id = parts.[0][5..] |> int
+  let numberParts = parts.[1].Trim().Split('|')
+  let winning = parseNumbers numberParts.[0]
+  let numbers = parseNumbers numberParts.[1]
+
+  { Id = id; Winning = winning; Numbers = numbers; }
+
+// --------------------------------------------------------
 // part1
 
-let lines = System.IO.File.ReadLines "sample.txt"
-//let lines = System.IO.File.ReadLines "input.txt"
+let cards =
+  lines |> Seq.map (fun l -> parseCard l)
+
+let winningNumberPoints card =
+  let winningNumbers = card.Numbers |> Array.filter (fun n -> card.Winning |> Array.contains n)
+  (2.0 ** winningNumbers.Length) / 2.0 |> int
+
+let winningNumbersSum =
+  cards 
+  |> Seq.map (fun c -> winningNumberPoints c)
+  |> Seq.sum
+
+printf "Sum of Winning Number points: %A" winningNumbersSum
+
+// --------------------------------------------------------
+// part2
+
+
