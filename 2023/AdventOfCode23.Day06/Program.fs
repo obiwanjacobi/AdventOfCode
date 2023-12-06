66dee111d514d732dc9e0ea9f9d4172bbed9a16d
Marc Jacobi <Marc.Jacobi@macaw.nl>
2023-12-06 13:37:46 +0100

    day 6 part 2

--- 2023/AdventOfCode23.Day06/Program.fs
+++ 2023/AdventOfCode23.Day06/Program.fs
@@ -10,9 +10,6 @@ let times = lines.[0].Substring(11).Split(' ', StringSplitOptions.RemoveEmptyEnt
 let distances = lines.[1].Substring(11).Split(' ', StringSplitOptions.RemoveEmptyEntries) |> Array.map (fun t -> t |> int)
 let games = Array.zip times distances
 
-//printf "Times: %A" times
-//printf "Distances: %A" distances
-
 // --------------------------------------------------------
 // part1
 
@@ -32,3 +29,22 @@ printf "Product of Win counts: %A\r\n" winProduct
 
 // --------------------------------------------------------
 // part2
+
+// feels like there should be some kind of formula to get to the answer
+// but all I can think of is a brute force solution...
+
+// same as countWins but 64 bits
+let countWins2 (time: int64) (distance: int64) =
+  let times = int64(1) |> Seq.unfold (fun state ->
+    if state < time then Some (state, state + int64(1)) else None)
+  
+  times
+  |> Seq.map (fun t -> (time - t) * t) // rest-time * speed
+  |> Seq.filter (fun d -> d > distance)
+  |> Seq.length
+
+let time = lines.[0].Split(':').[1].Replace(" ", "") |> int64
+let distance = lines.[1].Split(':').[1].Replace(" ", "") |> int64
+let winCount = countWins2 time distance
+
+printf "Win count: %A\r\n" winCount
