21452eebe03faf986ecfe53835973e294c5b782f
Marc Jacobi <Marc.Jacobi@macaw.nl>
2023-12-08 16:24:20 +0100

    Day 7 part 2

--- 2023/AdventOfCode23.Day07/Program.fs
+++ 2023/AdventOfCode23.Day07/Program.fs
@@ -5,7 +5,7 @@ let lines = System.IO.File.ReadLines "input.txt"
 
 // input parsing
 
-let CardValues = [| '2'; '3'; '4'; '5'; '6'; '7'; '8'; '9'; 'T'; 'J'; 'Q'; 'K'; 'A' |]
+let CardValues1 = [| '2'; '3'; '4'; '5'; '6'; '7'; '8'; '9'; 'T'; 'J'; 'Q'; 'K'; 'A' |]
 
 type HandType = 
 | FiveOfaKind = 7
@@ -17,7 +17,7 @@ type HandType =
 | HighCard = 1
 | None = 0
 type Hand =
-  { Cards: string; Type: HandType; Bid: int; }
+  { Cards: string; Type: HandType; Bid: int; Jokers: int; AdjustedType: HandType; }
 
 let determineHandType cards =
   
@@ -41,34 +41,73 @@ let determineHandType cards =
   |> Seq.sortDescending
   |> Seq.fold (fun s l -> accumulateHandType s l) HandType.None
 
+let upgradeHandType handType jokerCount =
+  if jokerCount = 0 then
+    handType
+  else
+    match handType with
+    | HandType.HighCard when jokerCount = 1 -> HandType.OnePair
+    | HandType.OnePair when jokerCount = 1 || jokerCount = 2 -> HandType.ThreeOfaKind
+    | HandType.TwoPair when jokerCount = 1 -> HandType.FullHouse
+    | HandType.TwoPair when jokerCount = 2 -> HandType.FourOfaKind
+    | HandType.ThreeOfaKind when jokerCount = 1 || jokerCount = 3 -> HandType.FourOfaKind
+    | HandType.FullHouse when jokerCount = 2 || jokerCount = 3 -> HandType.FiveOfaKind
+    | HandType.FourOfaKind when jokerCount = 1 || jokerCount = 4 -> HandType.FiveOfaKind
+    | _ -> handType
+
 let parseHand (line: string) =
   let parts = line.Split(' ')
-  let handType = determineHandType parts[0]
-  { Cards = parts.[0]; Type = handType; Bid = int(parts.[1]); }
+  let cards = parts.[0]
+  let handType = determineHandType cards
+  let jokers = cards |> Seq.filter (fun c -> c = 'J') |> Seq.length
+  let jokerType = upgradeHandType handType jokers
+  { Cards = parts.[0]; Type = handType; Bid = int(parts.[1]); Jokers = jokers; AdjustedType = jokerType; }
 
 let hands = 
   lines 
   |> Seq.map (fun l -> parseHand l)
   |> Seq.toList
 
-//printf "%A\r\n" hands
+printf "%A\r\n" (hands |> List.filter (fun h -> h.Jokers > 0))
 
 // --------------------------------------------------------
 // part1
 
-let compareHands hand1 hand2 =
+let compareHands1 hand1 hand2 =
   if (hand1.Type = hand2.Type) then
     hand2.Cards |> Seq.compareWith (fun h1 h2 -> 
-      let i1 = CardValues |> Array.findIndex (fun c -> c = h1)
-      let i2 = CardValues |> Array.findIndex (fun c -> c = h2)
+      let i1 = CardValues1 |> Array.findIndex (fun c -> c = h1)
+      let i2 = CardValues1 |> Array.findIndex (fun c -> c = h2)
       i1 - i2) hand1.Cards
   else
     (int(hand1.Type) - int(hand2.Type))
 
 let winnings =
   hands
-  |> List.sortWith (fun h1 h2 -> compareHands h1 h2)
+  |> List.sortWith (fun h1 h2 -> compareHands1 h1 h2)
   |> List.mapi (fun i h -> h.Bid * (i + 1))
   |> List.sum
 
 printf "Winnings %A\r\n" winnings
+
+// --------------------------------------------------------
+// part2
+
+let CardValues2 = [| 'J'; '2'; '3'; '4'; '5'; '6'; '7'; '8'; '9'; 'T'; 'J'; 'Q'; 'K'; 'A' |]
+
+let compareHands2 hand1 hand2 =
+  if (hand1.AdjustedType = hand2.AdjustedType) then
+    hand2.Cards |> Seq.compareWith (fun h1 h2 -> 
+      let i1 = CardValues2 |> Array.findIndex (fun c -> c = h1)
+      let i2 = CardValues2 |> Array.findIndex (fun c -> c = h2)
+      i1 - i2) hand1.Cards
+  else
+    (int(hand1.AdjustedType) - int(hand2.AdjustedType))
+
+let jokerWinnings =
+  hands
+  |> List.sortWith (fun h1 h2 -> compareHands2 h1 h2)
+  |> List.mapi (fun i h -> h.Bid * (i + 1))
+  |> List.sum
+
+printf "Winnings %A\r\n" jokerWinnings
