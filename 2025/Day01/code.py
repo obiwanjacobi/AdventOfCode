bc263f626038658e81c957cceeeb70d3abac7f82
Marc Jacobi <Marc.Jacobi@macaw.nl>
2025-12-02 09:00:58 +0100

    fix day 1 part 2

--- 2025/Day01/code.py
+++ 2025/Day01/code.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 root = Path(__file__).parent
 input_path = root / "input.txt"
-#input_path = root / "sample.txt"
+#input_path = root / "sample2.txt"
 
 lines = []
 with input_path.open("r") as f:
@@ -23,59 +23,29 @@ for l in lines:
 
 print("Count1:", count1)
 
-total2 = 50
+dail2 = 50
 count2 = 0
 
-index = 0
+print(f"_   : {dail2} => 0")
+
 for l in lines:
-    prevTot = total2
+    prevDail = dail2
     prevCnt = count2
 
     val = int(l[1:])
     if l[0] == 'L':
-        total2 -= val
-        count2 += int(val / 100)
-        if prevTot > 0 and total2 <= 0:
-            count2 += 1
+        dail2 -= val % 100
     else:
-        total2 += val
-        count2 += int(val / 100)
-        if total2 >= 100:
+        dail2 += val % 100
+
+    count2 += int(val / 100)
+
+    if dail2 <= 0 or dail2 >= 100:
+        if prevDail != 0:
             count2 += 1
-
-    total2 = total2 % 100
-
-    if (prevCnt != count2):
-        print(f"{l}({index}):[{total2}] bounds +{count2 - prevCnt} | {count2}")
-    else:
-        print(f"{l}({index}):[{total2}] bounds")
-
-    # if total2 <= 0 or total2 >= 100:
-    #     prevCnt = count2
-
-    #     count2 += int(val / 100)
-    #     if prevTot != 0:
-    #         count2 += 1
-    #     total2 = total2 % 100
-
-    #     if (prevCnt != count2):
-    #         print(f"{l}({index}):[{total2}] bounds +{count2 - prevCnt} | {count2}")
-    #     else:
-    #         print(f"{l}({index}):[{total2}] bounds")
-    # else:
-    #     print(f"{l}({index}):[{total2}] in-bounds")
+        dail2 = dail2 % 100
     
-    index += 1
-
-#R50 => 0 (1)
-#R50 => 50
-#L50 => 0 (2)
-#L50 => 50
-#R75 => 25 (3)
-#L50 => 75 (4)
-#L25 => 50
-#L75 => 75 (5)
-#R50 => 25 (6)
+    print(f"{l}: {dail2} => {count2 - prevCnt} | {count2}")
 
 # to 0: increment
 # from 0: don't increment
@@ -83,6 +53,4 @@ for l in lines:
 # exceed lower bound: adjust
 # exceed upper bound: adjust
 
-# 5981 too low
-# 6410 too high
 print("Count2:", count2)
